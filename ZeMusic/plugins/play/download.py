a06b1b7cd2e9b111773848003f7a54cae3da4d41
fasdxoox <163231340+fasdxoox@users.noreply.github.com>
2025-01-02 03:48:50 +0300

    Update download.py

--- ZeMusic/plugins/play/download.py
+++ ZeMusic/plugins/play/download.py
@@ -91,7 +91,7 @@ async def song_downloader(client, message: Message):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton(text="🎂𝐋𝐚𝐫𝐢𝐧 𝐁𝐢𝐫𝐭𝐡𝐝𝐚𝐲🎂", url=lnk),
+                        InlineKeyboardButton(text="♪ 𝐋𝐚𝐫𝐢𝐧 ♪", url=lnk),
                     ],
                 ]
             ),
